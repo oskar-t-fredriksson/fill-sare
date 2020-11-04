579fb2584ad0cac49770c2b1c6f43c5f88ab7216
oskar-t-fredriksson <72459215+oskar-t-fredriksson@users.noreply.github.com>
2020-11-04 10:54:21 +0100

    Added code From: https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,25 @@ namespace filläsare
     {
         static void Main(string[] args)
         {
+            //C:\Users\oskar\exempelfil-sv.txt
             Console.WriteLine("Ange ett filnamn: ");
             string fileName = Console.ReadLine();
-            Console.WriteLine("{0}", fileName);
+            Console.WriteLine("Öppnar {0}", fileName);
+            //From: https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
+            // Read file using StreamReader. Reads file line by line    
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
         }
     }
 }
