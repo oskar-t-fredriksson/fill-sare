0417bad51b243d1559f0866bfbb65755a30f79a7
oskar-t-fredriksson <72459215+oskar-t-fredriksson@users.noreply.github.com>
2020-11-04 10:45:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filläsare
+{
+    //Fas 1. Uppgift 3 - Rad/Tecken-räknare
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett filnamn: ");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("{0}", fileName);
+        }
+    }
+}
